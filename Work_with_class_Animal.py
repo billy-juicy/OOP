e3027f9100df10dbdf6e1929b0f8c4263eb90231
billy-juicy <libertyeternity00@gmail.com>
2025-09-08 09:05:04 +0300

    Created the hierarchy of the animal class

---
+++ Work_with_class_Animal.py
@@ -0,0 +1,33 @@
+class Animal:
+
+    def __init__(self, color="white"):
+        self.color = color
+
+    def sound(self):
+        return "УУУУУУ"
+
+class Dog(Animal):
+
+    def __init__(self, color, big_size):
+        super().__init__(color)
+        self.big_size = big_size
+
+    def sound(self):
+        return "гав-гав"
+
+class Cat(Animal):
+
+    def __init__(self, color, small_size):
+        super().__init__(color)
+        self.small_size = small_size
+
+    def sound(self):
+        return "мяу-мяу"
+
+animal = Animal()
+dog = Dog("brown", True)
+cat = Cat("grey", False)
+
+print(animal.sound())
+print(cat.sound())
+print(dog.sound())
